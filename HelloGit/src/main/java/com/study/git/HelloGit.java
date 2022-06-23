c811387cf35e8935e643a1987d2e22e42930e76e
HuSiSong <2622431283@qq.com>
2022-06-23 09:59:30 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/study/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.study.git;
+
+/**
+ * @author hss
+ * @create 2022-06-23 9:47
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
